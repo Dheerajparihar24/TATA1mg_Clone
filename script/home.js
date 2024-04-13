44163d62f406550e5561c2cf656910cb4c9a7227
Dheerajparihar24 <dheerajparihar02001@gmail.com>
2024-04-13 22:15:44 +0530

    project

--- script/home.js
+++ script/home.js
@@ -1,23 +1,24 @@
-let slides = document.querySelectorAll('.slide');
-slides.forEach(function(el,index){
-    el.style.left = `${index * 100}%`;
-})
-let currentIndex = 0;
-function slideImage() {
-    // Move to the next slide
-    currentIndex++;
-    if (currentIndex >= slides.length) {
-        currentIndex = 0; // Reset to the first slide if at the end
-    }
+// let slides = document.querySelectorAll('.slide');
+// slides.forEach(function(el,index){
+//     el.style.left = `${index * 100}%`;
+// })
+// let currentIndex = 0;
+// function slideImage() {
+//     // Move to the next slide
+//     currentIndex++;
+//     if (currentIndex >= slides.length) {
+//         currentIndex = 0; // Reset to the first slide if at the end
+//     }
     
-    // Update positions of all slides based on currentIndex
-    slides.forEach(function(el, index) {
-        el.style.left = `${(index - currentIndex) * 100}%`;
-    });
-}
-setInterval(slideImage, 3000);
+//     // Update positions of all slides based on currentIndex
+//     slides.forEach(function(el, index) {
+//         el.style.left = `${(index - currentIndex) * 100}%`;
+//     });
+// }
+// setInterval(slideImage, 3000);
 
 // ---------------------------------------------------------------------------------------------------------
+
 
 
 
--- script/signUp.js
+++ script/signUp.js
@@ -1,23 +1,42 @@
-let slides = document.querySelectorAll('.slide');
-// console.log(slides);
-slides.forEach(function(el,index){
-    el.style.left = `${index * 100}%`;
-})
-let currentIndex = 0;
-function slideImage() {
-    // Move to the next slide
-    currentIndex++;
-    if (currentIndex >= slides.length) {
-        currentIndex = 0; // Reset to the first slide if at the end
-    }
+// let slides = document.querySelectorAll('.slide');
+// slides.forEach(function(el,index){
+//     el.style.left = `${index * 100}%`;
+// })
+
+// let currentIndex = 0;
+// function slideImage() { 
     
-    // Update positions of all slides based on currentIndex
-    slides.forEach(function(el, index) {
-        el.style.transition = 'left 1s ease'; // Add transition effect
-        el.style.left = `${(index - currentIndex) * 100}%`;
-    });
+//     currentIndex++;
+//     if (currentIndex >= slides.length) {
+//         currentIndex = 0; // Reset to the first slide if at the end
+//     }
+    
+//     // Update positions of all slides based on currentIndex
+//     slides.forEach(function(el, index) {
+//         el.style.transition = 'left 1s ease'; // Add transition effect
+//         el.style.left = `${(index - currentIndex) * 100}%`;
+//     });
+// }
+
+// setInterval(slideImage, 3000);
+
+
+// login----
+// var modal = document.getElementById('id01');
+
+
+
+let signIn = document.getElementById("id02")
+window.onclick = function(event) {
+    if (event.target == signIn) {
+        signIn.style.display = "none";
+    }
 }
 
-// Call slideImage() every 3 seconds
-setInterval(slideImage, 3000);
 
+let signUp = document.getElementById("id01")
+window.onclick = function(event) {
+    if (event.target == signUp) {
+        signUp.style.display = "none";
+    }
+}
